c7fdf252480ca4ad2c7e42a312dc131d3278f93c
Mike Arvela <mike@arvela.net>
2012-02-09 13:03:49 +0200

    Fetching & dumping data in view

--- src/com/nokia/luinjo/LuinjoMainActivity.java
+++ src/com/nokia/luinjo/LuinjoMainActivity.java
@@ -9,13 +9,16 @@ import java.net.URISyntaxException;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
+import org.apache.http.StatusLine;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 public class LuinjoMainActivity extends Activity {
 		
@@ -27,56 +30,85 @@ public class LuinjoMainActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+                
+        TextView tv;
+        tv = (TextView) findViewById(R.id.textview);
+        tv.setText("Loading...");
         
-        System.out.println(getTopStories());
+        String contentStr = getTopStories();
+        if (contentStr == null) {
+        	contentStr = "(no data)";
+        }                
+        tv.setText(contentStr);        
     }
     
-    // TODO: create new thread
     private String getTopStories() {
-    	HttpResponse response = null;
-    	
+    	return getContent("http://www.reddit.com/.json");
+    }
+
+    private String getContent(String url) {
+    	HttpGet request = new HttpGet();    	
+		try {
+			URI uri = new URI(url);
+			request.setURI(uri);
+		} catch (URISyntaxException e) {
+			Log.e(TAG, "Invalid URL: " + e.getMessage());
+			return null;
+		}
+    	return getContent(request);
+    }
+    
+    // TODO: do data fetching in separate thread
+    // TODO: configure charset etc. for client
+    private String getContent(HttpUriRequest request) {
+    	HttpResponse response = null;    	
     	try {        	
-        	HttpGet request = new HttpGet();
-        	// request.setURI(new URI(REDDIT_BASE_URL + ".json"));
-        	request.setURI(new URI("http://www.futurice.com"));
         	HttpClient client = new DefaultHttpClient();        	    
         	response = client.execute(request);        	    		
     	} catch (IOException e) {
     		Log.e(TAG, "IOException executing HTTP request: " + e.getMessage());
-    	} catch (URISyntaxException e) {
-    		Log.e(TAG, "Invalid URI syntax: " + e.getMessage());
-			e.printStackTrace();
-		}
+    		return null;
+    	}
+    	
+    	StatusLine statusLine = response.getStatusLine();
+    	if (statusLine.getStatusCode() != 200) {
+    		Log.e(TAG, "Request wasn't completed successfully (status code " + statusLine.getStatusCode() + ")");
+    		return null;
+    	}
     	
     	HttpEntity entity = response.getEntity();
     	String content = null;
-    	if (entity != null) {
-    		InputStream in = null;
+    	if (entity == null) {
+    		return null;
+    	}
+    	
+		InputStream in = null;
+		try {
+			in = entity.getContent();
+			content = getAsString(in);
+		} catch (IllegalStateException e) {
+			Log.e(TAG, "Illegal state encountered reading content: " + e.getMessage());
+		} catch (IOException e) {
+			Log.e(TAG, "I/O error reading content: " + e.getMessage());
+		} finally {
 			try {
-				in = entity.getContent();
-				content = getAsString(in);
-			} catch (IllegalStateException e) {
-				Log.e(TAG, "Illegal state encountered reading content: " + e.getMessage());
-			} catch (IOException e) {
-				Log.e(TAG, "I/O error reading content: " + e.getMessage());
-			} finally {
-				try {
-					if (in != null) {
-						in.close();
-					}
-				}
-				catch (IOException ioe) {
-					Log.e(TAG, "Could not close input stream: " + ioe.getMessage());
+				if (in != null) {
+					in.close();
 				}
 			}
-    	}    	
-    	return content;
+			catch (IOException ioe) {
+				Log.e(TAG, "Could not close input stream: " + ioe.getMessage());
+			}
+		}
+		
+    	return content;    	
     }
     
     private String getAsString(InputStream in) {		
 		StringBuilder sb = new StringBuilder();		
 		String line = null;
 		BufferedReader br = new BufferedReader(new InputStreamReader(in));
+		
 		try {						
 			while ((line = br.readLine()) != null) {
 				sb.append(line + "\n");
