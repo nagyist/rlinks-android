9797c99ab186cc93f74f429feba735b9adb8cfa5
Mike Arvela <mike@arvela.net>
2012-02-16 12:50:49 +0200

    Loading inline image in a non-blocking thread

--- src/com/nokia/luinjo/LuinjoDetailsActivity.java
+++ src/com/nokia/luinjo/LuinjoDetailsActivity.java
@@ -1,3 +1,4 @@
+
 package com.nokia.luinjo;
 
 import java.io.BufferedInputStream;
@@ -21,86 +22,99 @@ import com.nokia.luinjo.reddit.RedditLinkItem;
 import com.nokia.luinjo.reddit.RedditLinkItemView;
 
 public class LuinjoDetailsActivity extends Activity {
-	
-	private static final String TAG = "DetailsActivity";
 
-	/**
-	 * Called when the activity is first created.
-	 */
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		
-		RedditLinkItem item = (RedditLinkItem) getIntent().getSerializableExtra(RedditLinkItem.class.getName());
-		Log.d(TAG, "Received an item: " + item.toString());
-		
-		setContentView(R.layout.details);
-		RedditLinkItemView itemView = (RedditLinkItemView) findViewById(R.id.link_item);
-		itemView.populateWith(item);
-		
-		showOrHideImage(item);
-		loadComments(item);
-	}
-	
-	private boolean isImage(String url) {
-		return (url.endsWith(".jpg") || url.endsWith(".png") || url.endsWith(".gif"));
-	}
-	
-	private void showOrHideImage(RedditLinkItem item) {
-		ImageView imageView = (ImageView) findViewById(R.id.link_image);
-		
-		boolean isImage = isImage(item.getUrl());
-		imageView.setVisibility(isImage ? View.VISIBLE : View.INVISIBLE);		
-		if (!isImage) {			
-			return;
-		}
+    private static final String TAG = "DetailsActivity";
 
-		// Attempt to load and show image
-		Bitmap imageBitmap = getImageBitmapFromUrl(item.getUrl());
-		if (imageBitmap != null) {
-			imageView.setImageBitmap(imageBitmap);
-		}
-	}
-	
-	private void loadComments(RedditLinkItem item) {
-		RedditClient client = new RedditClient();
-		client.getComments(item);
-	}
-	
-	private Bitmap getImageBitmapFromUrl(String imageUrlStr) {
-		URL imageUrl = null;
-	    HttpURLConnection connection = null;
-	    InputStream is = null;
-	    BufferedInputStream buffer = null;
-	    Bitmap bitmap = null;
-		try {
-			imageUrl = new URL(imageUrlStr);
-		    connection = (HttpURLConnection) imageUrl.openConnection();
-		    is = connection.getInputStream();
-		    buffer = new BufferedInputStream(is);
-		    bitmap = BitmapFactory.decodeStream(buffer);
-		} catch (MalformedURLException e) {
-			e.printStackTrace();
-		} catch (IOException ioe) {
-			ioe.printStackTrace();
-		}
-		finally {
-			try {
-				if (is != null) {
-					is.close(); 
-				}
-				if (buffer != null) {
-					buffer.close();
-				}
-			} catch (IOException e) {
-				e.printStackTrace();
-			}	
-		}
-		return bitmap;
-	}
-	
-	@Override
-    public void onConfigurationChanged(Configuration newConfig){
+    private ImageView mImageView;
+
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        RedditLinkItem item = (RedditLinkItem) getIntent().getSerializableExtra(
+                RedditLinkItem.class.getName());
+        Log.d(TAG, "Received an item: " + item.toString());
+
+        setContentView(R.layout.details);
+        mImageView = (ImageView) findViewById(R.id.link_image);
+
+        RedditLinkItemView itemView = (RedditLinkItemView) findViewById(R.id.link_item);
+        itemView.populateWith(item);
+
+        showOrHideImage(item);
+        // loadComments(item);
+    }
+
+    private boolean isImage(String url) {
+        return (url.endsWith(".jpg") || url.endsWith(".png") || url.endsWith(".gif"));
+    }
+
+    private void showOrHideImage(final RedditLinkItem item) {
+        // If the URL looks like an image, show the ImageView; otherwise hide it
+        boolean isImage = isImage(item.getUrl());
+        mImageView.setVisibility(isImage ? View.VISIBLE : View.INVISIBLE);
+        if (!isImage) {
+            return;
+        }
+
+        // Load the image bitmap in a non-blocking thread
+        new Thread(new Runnable() {
+            public void run() {
+                final Bitmap imageBitmap = getImageBitmapFromUrl(item.getUrl());
+                if (imageBitmap != null) {
+                    mImageView.post(new Runnable() {
+                        public void run() {
+                            mImageView.setImageBitmap(imageBitmap);
+                        }
+                    });
+                }
+            }
+        }).start();
+    }
+
+    private void loadComments(RedditLinkItem item) {
+        RedditClient client = new RedditClient();
+        client.getComments(item);
+    }
+
+    private Bitmap getImageBitmapFromUrl(String imageUrlStr) {
+        URL imageUrl = null;
+        HttpURLConnection connection = null;
+        InputStream is = null;
+        BufferedInputStream buffer = null;
+        Bitmap bitmap = null;
+        
+        try {
+            imageUrl = new URL(imageUrlStr);
+            connection = (HttpURLConnection) imageUrl.openConnection();
+            is = connection.getInputStream();
+            buffer = new BufferedInputStream(is);
+            bitmap = BitmapFactory.decodeStream(buffer);
+        } catch (MalformedURLException e) {
+            Log.e(TAG, "Invalid URL: " + e.getMessage());
+        } catch (IOException ioe) {
+            Log.e(TAG, "I/O exception on trying to load image: " + ioe.getMessage());
+        } finally {
+            try {
+                if (is != null) {
+                    is.close();
+                }
+                if (buffer != null) {
+                    buffer.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        
+        return bitmap;
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
         super.onConfigurationChanged(newConfig);
-	}	
+    }
 }
