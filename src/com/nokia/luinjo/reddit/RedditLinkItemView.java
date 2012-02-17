28998ca30725532d89cdfdfbab6ed0eb0582cbe1
Mike Arvela <mike@arvela.net>
2012-02-17 17:45:44 +0200

    Showing thumbnail images and when possible, hiding them for more text space

--- src/com/nokia/luinjo/reddit/RedditLinkItemView.java
+++ src/com/nokia/luinjo/reddit/RedditLinkItemView.java
@@ -1,6 +1,7 @@
 package com.nokia.luinjo.reddit;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.ImageView;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
+import com.nokia.luinjo.LuinjoHttpClient;
 import com.nokia.luinjo.R;
 
 public class RedditLinkItemView extends RelativeLayout {
@@ -46,21 +48,39 @@ public class RedditLinkItemView extends RelativeLayout {
 	}
 
 	public void populateWith(RedditLinkItem item) {
-        ImageView thumbnailView = (ImageView) this.findViewById(R.id.thumbnail);
-        String thumbnailUrl = item.getThumbnail();
-        if (thumbnailUrl == null || thumbnailUrl.equals("")) {
-            thumbnailView.setVisibility(View.INVISIBLE);
-        }
-        
-        // TODO: get image in background
-        // thumbnailView.setImageURI(Uri.parse(thumbnailUrl));
+	    showOrHideThumbnail(item);
         
 		getTextView(R.id.title).setText(item.getTitle());
 		getTextView(R.id.score).setText("" + item.getScore());
 		getTextView(R.id.domain).setText(item.getDomain());
 		getTextView(R.id.num_comments).setText(
 				"" + item.getNumComments() + " " + mContext.getResources().getString(R.string.comments));
-		getTextView(R.id.subreddit).setText(item.getSubreddit());
-		
+		getTextView(R.id.subreddit).setText(item.getSubreddit());		
+	}
+	
+	private void showOrHideThumbnail(RedditLinkItem item) {
+        ImageView thumbnailView = (ImageView) this.findViewById(R.id.thumbnail);
+        String thumbnailUrl = item.getThumbnail();        
+        if (thumbnailUrl == null || thumbnailUrl.equals("")) {
+            thumbnailView.setVisibility(View.GONE);
+        } else {
+            showThumbnailImage(thumbnailView, thumbnailUrl);
+        }	    
+	}
+	
+	private void showThumbnailImage(final ImageView thumbnailView, final String thumbnailUrl) {
+        new Thread(new Runnable() {
+            public void run() {
+                final LuinjoHttpClient httpClient = new LuinjoHttpClient();
+                final Bitmap imageBitmap = httpClient.getImageBitmapFromUrl(thumbnailUrl);
+                if (imageBitmap != null) {
+                    thumbnailView.post(new Runnable() {
+                        public void run() {
+                            thumbnailView.setImageBitmap(imageBitmap);
+                        }
+                    });
+                }
+            }
+        }).start();	    
 	}
 }
